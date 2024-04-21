e385d7ed60417bd8b9fd024c0f6ee64c86b0416b
Kegen Guyll <guyllkegen@gmail.com>
2024-04-21 17:10:30 -0500

    Refactor search screen and components for improved functionality and UI

--- app/(tabs)/(movies,search)/search/index.tsx
+++ app/(tabs)/(movies,search)/search/index.tsx
@@ -1,39 +1,126 @@
 import { Stack } from 'expo-router';
-import React from 'react';
-import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import React, { useEffect } from 'react';
+import {
+	NativeSyntheticEvent,
+	TextInputChangeEventData,
+	StyleSheet,
+	View,
+	ScrollView,
+	FlatList,
+	LogBox,
+} from 'react-native';
+import { Chip, Divider, List, Searchbar, Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useFindMoviesQuery } from '@/redux/api/tmdb/searchApi';
+import { useFindMoviesQuery, useFindPeopleQuery } from '@/redux/api/tmdb/searchApi';
 import MovieGrid from '@/components/MovieGrid';
+import ActorPlaceholderImage from '@/components/ActorPlacholderImage';
 
-const Search = () => {
+type SearchSelection = 'movies' | 'crew';
+
+type SearchSelectionChipProps = {
+	label: string;
+	onPress: () => void;
+	isSelected: boolean;
+};
+
+const SearchSelectionChip: React.FC<SearchSelectionChipProps> = ({
+	label,
+	onPress,
+	isSelected,
+}) => {
+	return (
+		<Chip onPress={onPress} mode={!isSelected ? 'outlined' : 'flat'}>
+			<Text>{label}</Text>
+		</Chip>
+	);
+};
+
+const Search: React.FC = () => {
 	const [searchQuery, setSearchQuery] = React.useState('');
+	const [searchSelection, setSearchSelection] = React.useState<SearchSelection>('movies');
 
 	const onChangeSearch = (e: NativeSyntheticEvent<TextInputChangeEventData>) =>
 		setSearchQuery(e.nativeEvent.text);
 
-	const { data, isFetching } = useFindMoviesQuery(
+	const { data: movieData, isFetching: movieIsFetching } = useFindMoviesQuery(
 		{ query: searchQuery },
-		{ skip: searchQuery.length < 3 }
+		{ skip: searchSelection !== 'movies' }
 	);
+
+	const { data: peopleData, isFetching: peopleIsFetching } = useFindPeopleQuery(
+		{ query: searchQuery },
+		{ skip: searchSelection !== 'crew' }
+	);
+	useEffect(() => {
+		LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+	}, []);
 
 	return (
 		<>
 			<Stack.Screen options={{ headerShown: false }} />
 			<SafeAreaView>
 				<Searchbar placeholder="Search" value={searchQuery} onChange={onChangeSearch} />
-				{data && (
-					<MovieGrid
-						movies={data.results}
-						isLoading={isFetching}
-						onEndReached={() => {}}
-						onEndReachedThreshold={1}
-						posterPath="search"
+				{/* <ScrollView horizontal> */}
+				<View style={styles.chipContainer}>
+					<SearchSelectionChip
+						label="Movies"
+						onPress={() => setSearchSelection('movies')}
+						isSelected={searchSelection === 'movies'}
 					/>
-				)}
+					<SearchSelectionChip
+						label="Cast, Crew or Studios"
+						onPress={() => setSearchSelection('crew')}
+						isSelected={searchSelection === 'crew'}
+					/>
+				</View>
+				{/* </ScrollView> */}
+				<ScrollView>
+					{movieData && searchSelection === 'movies' && (
+						<MovieGrid
+							movies={movieData.results}
+							isLoading={movieIsFetching}
+							onEndReached={() => {}}
+							onEndReachedThreshold={1}
+							posterPath="search"
+						/>
+					)}
+				</ScrollView>
+				<ScrollView>
+					{peopleData && searchSelection === 'crew' && (
+						<FlatList
+							data={peopleData.results}
+							renderItem={({ item }) => (
+								<>
+									<List.Item
+										title={item.name}
+										left={() => (
+											<ActorPlaceholderImage
+												profile_url={item.profile_path}
+												department={item.known_for_department}
+												height={50}
+												width={50}
+											/>
+										)}
+										description={item.known_for_department}
+									/>
+									<Divider />
+								</>
+							)}
+							keyExtractor={(item) => item.id.toString()}
+						/>
+					)}
+				</ScrollView>
 			</SafeAreaView>
 		</>
 	);
 };
+
+const styles = StyleSheet.create({
+	chipContainer: {
+		flexDirection: 'row',
+		gap: 8,
+		marginTop: 8,
+	},
+});
 
 export default Search;
