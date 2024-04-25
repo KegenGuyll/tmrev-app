65be0384da73c96ced9c7f741d42820953a5595a
Kegen Guyll <guyllkegen@gmail.com>
2024-04-25 16:09:21 -0500

    Add TMREV user creation on Google Sign-In

--- app/login.tsx
+++ app/login.tsx
@@ -1,12 +1,53 @@
 import { View } from 'react-native';
 import { Text, Button, TextInput, Divider } from 'react-native-paper';
-import auth from '@react-native-firebase/auth';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import { GOOGLE_WEB_CLIENT_ID } from '@env';
+import { GOOGLE_WEB_CLIENT_ID, TMREV_API_URL } from '@env';
 import { useRouter } from 'expo-router';
+
+type CreateTMREVUser = {
+	bio: string;
+	email: string;
+	firstName: string;
+	lastName: string;
+	link: string | null;
+	location: string;
+	photoUrl: string | null;
+	public: boolean;
+	uuid: string;
+};
 
 const Login: React.FC = () => {
 	const router = useRouter();
+
+	const findUserByUid = async (uid: string) => {
+		const res = await fetch(`${TMREV_API_URL}/user/isUser/${uid}`);
+		const data = await res.json();
+
+		return data;
+	};
+
+	const createTMREVUser = async (currentUser: FirebaseAuthTypes.User) => {
+		const token = await currentUser.getIdToken();
+		await fetch(`${TMREV_API_URL}/user`, {
+			method: 'POST',
+			headers: {
+				authorization: token,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				uuid: currentUser.uid,
+				email: currentUser.email,
+				firstName: auth().currentUser?.displayName?.split(' ')[0],
+				lastName: auth().currentUser?.displayName?.split(' ')[1],
+				photoUrl: currentUser.photoURL,
+				bio: '',
+				location: '',
+				link: null,
+				public: true,
+			} as CreateTMREVUser),
+		});
+	};
 
 	const onGoogleButtonPress = async () => {
 		try {
@@ -23,9 +64,19 @@ const Login: React.FC = () => {
 
 			// Sign-in the user with the credential
 			await auth().signInWithCredential(googleCredential);
+			const { currentUser } = auth();
+
+			if (currentUser) {
+				const dbUser = await findUserByUid(currentUser?.uid);
+
+				if (!dbUser.isUser) {
+					await createTMREVUser(currentUser);
+				}
+			}
+
 			router.replace('/(tabs)/(home)/home');
 		} catch (error) {
-			console.error(error);
+			console.error(error, 'error');
 		}
 	};
 
