c11739075d14af598b8e0f3abafd02d71887f45a
Kegen Guyll <guyllkegen@gmail.com>
2024-05-01 13:18:17 -0500

    moved profile location

--- app/(tabs)/(movies,search,home,add-review,profile)/profile/[profileId]/index.tsx
+++ app/(tabs)/(movies,search,home,add-review,profile)/profile/[profileId]/index.tsx
@@ -1,18 +1,22 @@
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import { View } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import { IconButton, Menu, Text } from 'react-native-paper';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import ProfileHeader from '@/components/Profile/ProfileHeader';
 import { useGetUserQuery } from '@/redux/api/tmrev';
+import ClickableSurface from '@/components/ClickableSurface';
+import { useAppDispatch } from '@/hooks/reduxHooks';
+import { setUserProfile } from '@/redux/slice/userProfileSlice';
 
-type ProfileSearchParams = {
+export type ProfileSearchParams = {
 	profileId: string;
+	from?: string;
 };
 
 const Profile = () => {
 	const { currentUser } = auth();
-	const { profileId } = useLocalSearchParams<ProfileSearchParams>();
+	const { profileId, from } = useLocalSearchParams<ProfileSearchParams>();
 	const { data: profileData, isLoading: isProfileLoading } = useGetUserQuery(
 		{ uid: profileId },
 		{ skip: !profileId }
@@ -22,9 +26,17 @@ const Profile = () => {
 		{ skip: !currentUser || !currentUser.uid }
 	);
 	const [visible, setVisible] = useState(false);
+	const dispatch = useAppDispatch();
+	const router = useRouter();
 
 	const openMenu = () => setVisible(true);
 	const closeMenu = () => setVisible(false);
+
+	useEffect(() => {
+		if (profileData) {
+			dispatch(setUserProfile(profileData));
+		}
+	}, [profileData]);
 
 	const isFollowing = useMemo(() => {
 		if (profileData && loggedInUserData) {
@@ -64,6 +76,11 @@ const Profile = () => {
 			/>
 			<View>
 				<ProfileHeader followVisible user={profileData} isCurrentlyFollowing={isFollowing} />
+				<ClickableSurface
+					onPress={() => router.push(`/(tabs)/(${from})/profile/${profileId}/allReviews`)}
+					title="View All Reviews"
+					icon="chevron-right"
+				/>
 			</View>
 		</>
 	);
