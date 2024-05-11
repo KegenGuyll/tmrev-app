8c4dbdc8c97f9a05a2ca8a523313908ad5021439
Kegen Guyll <guyllkegen@gmail.com>
2024-05-11 13:53:22 -0500

    created a titledSlider component

---
+++ components/TitledSlider.tsx
@@ -0,0 +1,38 @@
+import { View } from 'react-native';
+import { Text, useTheme } from 'react-native-paper';
+import { Slider } from '@miblanchard/react-native-slider';
+
+type TitledSliderProps = {
+	title: string;
+	value: number;
+	onValueChange: (value: number) => void;
+};
+
+const TitledSlider: React.FC<TitledSliderProps> = ({
+	title,
+	value,
+	onValueChange,
+}: TitledSliderProps) => {
+	const theme = useTheme();
+
+	return (
+		<View style={{ paddingLeft: 16, paddingRight: 16, gap: 1 }}>
+			<Text variant="labelLarge">
+				{title}: {value}
+			</Text>
+			<Slider
+				step={1}
+				value={value}
+				maximumValue={10}
+				minimumValue={1}
+				thumbTintColor={theme.colors.primary}
+				maximumTrackTintColor={theme.colors.secondaryContainer}
+				minimumTrackTintColor={theme.colors.secondary}
+				onValueChange={(v) => onValueChange(Number(v))}
+				animationType="timing"
+			/>
+		</View>
+	);
+};
+
+export default TitledSlider;
