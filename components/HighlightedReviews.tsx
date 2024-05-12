fb6e7d7daf14a9f4dc1778acd28d99a8dc7ebf77
Kegen Guyll <guyllkegen@gmail.com>
2024-05-11 21:14:11 -0500

    feat: Add HighlightedReviews component for displaying highest and lowest rated movies

---
+++ components/HighlightedReviews.tsx
@@ -0,0 +1,88 @@
+import { View, StyleSheet } from 'react-native';
+import { Chip, Text, TouchableRipple } from 'react-native-paper';
+import { FlatGrid } from 'react-native-super-grid';
+import { useGetUserHighlightedReviewsQuery } from '@/redux/api/tmrev';
+import { FromLocation } from '@/models';
+import MoviePoster from './MoviePoster';
+import { TmrevReview } from '@/models/tmrev';
+
+type HighlightedReviewsProps = {
+	userId?: string;
+	from: FromLocation;
+};
+
+type HighlightedReviewItemProps = {
+	movie: TmrevReview;
+	from: FromLocation;
+};
+
+const HighlightedReviewItem: React.FC<HighlightedReviewItemProps> = ({
+	movie,
+	from,
+}: HighlightedReviewItemProps) => {
+	return (
+		<TouchableRipple style={{ position: 'relative' }}>
+			<>
+				<Chip style={{ position: 'absolute', bottom: 4, right: 8, zIndex: 1 }} icon="star">
+					{movie.averagedAdvancedScore}
+				</Chip>
+				<MoviePoster movieId={movie.tmdbID} moviePoster={movie.moviePoster} location={from} />
+			</>
+		</TouchableRipple>
+	);
+};
+
+const HighlightedReviews: React.FC<HighlightedReviewsProps> = ({
+	userId,
+	from,
+}: HighlightedReviewsProps) => {
+	if (!userId) return null;
+
+	const { data } = useGetUserHighlightedReviewsQuery(userId, { skip: !userId });
+
+	if (!data) return null;
+
+	return (
+		<View>
+			<View>
+				<Text style={{ padding: 8 }} variant="labelMedium">
+					Highest Rated Movies ({data.body.highest.length})
+				</Text>
+				<FlatGrid
+					itemDimension={100}
+					style={styles.list}
+					data={data.body.highest}
+					spacing={8}
+					renderItem={({ item }) => <HighlightedReviewItem movie={item} from={from} />}
+					keyExtractor={(item) => item._id}
+				/>
+			</View>
+			<View>
+				<Text style={{ padding: 8 }} variant="labelMedium">
+					Lowest Rated Movies ({data.body.lowest.length})
+				</Text>
+				<FlatGrid
+					itemDimension={100}
+					style={styles.list}
+					data={data.body.lowest}
+					spacing={8}
+					renderItem={({ item }) => <HighlightedReviewItem movie={item} from={from} />}
+					keyExtractor={(item) => item._id}
+				/>
+			</View>
+		</View>
+	);
+};
+
+export default HighlightedReviews;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	list: {
+		display: 'flex',
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+	},
+});
