36cc2e591e13797d9e963e5fb5c64b64ca909962
Kegen Guyll <guyllkegen@gmail.com>
2024-05-21 16:24:06 -0500

    added support for original imageUrl size

--- utils/imageUrl.ts
+++ utils/imageUrl.ts
@@ -1,4 +1,8 @@
-const imageUrl = (path: string, w?: number, resized = true) => {
+const imageUrl = (path: string, w?: number | string, resized = true) => {
+	if (w === 'original') {
+		return `https://image.tmdb.org/t/p/original${path}`;
+	}
+
 	if (resized && w) {
 		return `https://image.tmdb.org/t/p/w${w}${path}`;
 	}
