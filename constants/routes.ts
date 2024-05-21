41b3aecd70a05d29f1b7c0cd800a335308c66288
Kegen Guyll <guyllkegen@gmail.com>
2024-05-21 18:47:21 -0500

    create listDetailRoute

--- constants/routes.ts
+++ constants/routes.ts
@@ -16,6 +16,10 @@ export const allListsRoute = (from: FromLocation, profileId: string) => {
 	return `${baseTabUrl}/(${from})/profile/${profileId}/allLists?from=${from}`;
 };
 
+export const listDetailsRoute = (from: FromLocation, listId: string, profileId: string) => {
+	return `${baseTabUrl}/(${from})/list/${listId}?from=${from}&profileId=${profileId}`;
+};
+
 export const movieReviewsRoute = (from: FromLocation, movieId: string, movieTitle: string) => {
 	return `${baseTabUrl}/(${from})/reviews/${movieId}?from=${from}&title=${movieTitle}`;
 };
