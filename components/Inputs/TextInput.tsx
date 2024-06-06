af14c9d140fb6d83fbb4f27d9b4e373bdfeccee0
Kegen Guyll <guyllkegen@gmail.com>
2024-06-05 21:24:58 -0500

    refactor: Update MultiLineInput and TextInput components to use forwardRef

--- components/Inputs/TextInput.tsx
+++ components/Inputs/TextInput.tsx
@@ -10,44 +10,45 @@ type TextInputProps = {
 	onFocus?: () => void;
 };
 
-const TextInput: React.FC<TextInputProps> = ({
-	placeholder,
-	label,
-	value,
-	onChangeText,
-	onFocus,
-}: TextInputProps) => {
-	const theme = useTheme();
+const TextInput = React.forwardRef(
+	(
+		{ placeholder, label, value, onChangeText, onFocus }: TextInputProps,
+		ref: React.LegacyRef<NativeTextInput>
+	) => {
+		const theme = useTheme();
 
-	if (Platform.OS === 'ios') {
+		if (Platform.OS === 'ios') {
+			return (
+				<View style={{ gap: 4 }}>
+					{label && <Text variant="labelLarge">{label}</Text>}
+					<NativeTextInput
+						ref={ref}
+						style={{
+							padding: 8,
+							backgroundColor: theme.colors.surfaceVariant,
+							borderRadius: 4,
+							color: theme.colors.onSurfaceVariant,
+						}}
+						editable
+						defaultValue={value}
+						onFocus={onFocus}
+						onChange={(e) => onChangeText(e.nativeEvent.text)}
+					/>
+				</View>
+			);
+		}
+
 		return (
-			<View style={{ gap: 4 }}>
-				<Text variant="labelLarge">{label}</Text>
-				<NativeTextInput
-					style={{
-						padding: 8,
-						backgroundColor: theme.colors.surfaceVariant,
-						borderRadius: 4,
-						color: theme.colors.onSurfaceVariant,
-					}}
-					editable
-					defaultValue={value}
-					onFocus={onFocus}
-					onChange={(e) => onChangeText(e.nativeEvent.text)}
-				/>
-			</View>
+			<PaperTextInput
+				ref={ref}
+				placeholder={placeholder}
+				onFocus={onFocus}
+				value={value}
+				onChangeText={onChangeText}
+				label={label}
+			/>
 		);
 	}
-
-	return (
-		<PaperTextInput
-			placeholder={placeholder}
-			onFocus={onFocus}
-			value={value}
-			onChangeText={onChangeText}
-			label={label}
-		/>
-	);
-};
+);
 
 export default TextInput;
