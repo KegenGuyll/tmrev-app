1a3df9d35b39fe7ea1332ad04774370682ea1d09
Kegen Guyll <guyllkegen@gmail.com>
2024-06-11 16:15:13 -0500

    refactor: Update login page UI and add authentication functionality

--- app/login.tsx
+++ app/login.tsx
@@ -1,104 +1,94 @@
 import { View } from 'react-native';
-import { Text, Button, TextInput, Divider } from 'react-native-paper';
-import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
-import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import { GOOGLE_WEB_CLIENT_ID, TMREV_API_URL } from '@env';
-import { useRouter } from 'expo-router';
-
-type CreateTMREVUser = {
-	bio: string;
-	email: string;
-	firstName: string;
-	lastName: string;
-	link: string | null;
-	location: string;
-	photoUrl: string | null;
-	public: boolean;
-	uuid: string;
-};
+import { Text, Button, TextInput, Divider, Snackbar } from 'react-native-paper';
+import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import { AppleButton } from '@invertase/react-native-apple-authentication';
+import { Stack, useRouter } from 'expo-router';
+import { useState } from 'react';
+import useAuth from '@/hooks/useAuth';
+import { loggedInProfileRoute } from '@/constants/routes';
 
 const Login: React.FC = () => {
 	const router = useRouter();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [showPassword, setShowPassword] = useState(false);
+	const [errorMessage, setErrorMessage] = useState<string | null>('');
 
-	const findUserByUid = async (uid: string) => {
-		const res = await fetch(`${TMREV_API_URL}/user/isUser/${uid}`);
-		const data = await res.json();
+	const { onGoogleSignInButtonPress, emailSignIn, onAppleSignInButtonPress } = useAuth({
+		onSuccessfulSignIn: () => {
+			router.replace(loggedInProfileRoute('profile'));
+		},
+		onError: setErrorMessage,
+	});
 
-		return data;
-	};
-
-	const createTMREVUser = async (currentUser: FirebaseAuthTypes.User) => {
-		const token = await currentUser.getIdToken();
-		await fetch(`${TMREV_API_URL}/user`, {
-			method: 'POST',
-			headers: {
-				authorization: token,
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				uuid: currentUser.uid,
-				email: currentUser.email,
-				firstName: auth().currentUser?.displayName?.split(' ')[0],
-				lastName: auth().currentUser?.displayName?.split(' ')[1],
-				photoUrl: currentUser.photoURL,
-				bio: '',
-				location: '',
-				link: null,
-				public: true,
-			} as CreateTMREVUser),
-		});
-	};
-
-	const onGoogleButtonPress = async () => {
-		try {
-			GoogleSignin.configure({
-				webClientId: GOOGLE_WEB_CLIENT_ID,
-			});
-			// Check if your device supports Google Play
-			await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-			// Get the users ID token
-			const { idToken } = await GoogleSignin.signIn();
-
-			// Create a Google credential with the token
-			const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-			// Sign-in the user with the credential
-			await auth().signInWithCredential(googleCredential);
-			const { currentUser } = auth();
-
-			if (currentUser) {
-				const dbUser = await findUserByUid(currentUser?.uid);
-
-				if (!dbUser) {
-					await createTMREVUser(currentUser);
-				}
-			}
-
-			router.replace('/(tabs)/(profile)/profile');
-		} catch (error) {
-			console.error(error, 'error');
+	const onEmailSignIn = async () => {
+		if (!email || !password) {
+			setErrorMessage('Please enter your email and password');
+			return;
 		}
+
+		await emailSignIn(email, password);
 	};
 
 	return (
-		<View style={{ gap: 16, padding: 16 }}>
-			<View style={{ gap: 8 }}>
-				<TextInput mode="outlined" label="Email" />
-				<TextInput mode="outlined" label="Password" />
-				<Text style={{ textAlign: 'right' }}>Forgot Password?</Text>
-				<Button mode="contained" onPress={onGoogleButtonPress}>
-					Login
-				</Button>
+		<>
+			<Stack.Screen options={{ title: 'Login' }} />
+			<View style={{ gap: 16, padding: 16 }}>
+				<View>
+					<Text variant="headlineLarge">Log In</Text>
+					<Text variant="titleSmall">Welcome back! Please enter your details.</Text>
+				</View>
+
+				<View style={{ gap: 16 }}>
+					<TextInput
+						spellCheck={false}
+						inputMode="email"
+						value={email}
+						onChangeText={(t) => setEmail(t)}
+						mode="outlined"
+						label="Email"
+					/>
+					<TextInput
+						secureTextEntry={!showPassword}
+						value={password}
+						onChangeText={(t) => setPassword(t)}
+						mode="outlined"
+						label="Password"
+						right={
+							<TextInput.Icon
+								onPress={() => setShowPassword(!showPassword)}
+								icon={!showPassword ? 'eye' : 'eye-off'}
+							/>
+						}
+					/>
+					<Text style={{ textAlign: 'right' }}>Forgot Password?</Text>
+					<Button mode="contained" onPress={onEmailSignIn}>
+						Login
+					</Button>
+				</View>
+				<Divider />
+				<View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 8 }}>
+					<GoogleSigninButton
+						style={{ width: '100%', height: 45 }}
+						size={GoogleSigninButton.Size.Wide}
+						color={GoogleSigninButton.Color.Light}
+						onPress={onGoogleSignInButtonPress}
+					/>
+					<AppleButton
+						buttonStyle={AppleButton.Style.WHITE}
+						buttonType={AppleButton.Type.SIGN_IN}
+						style={{
+							width: '100%',
+							height: 45,
+						}}
+						onPress={onAppleSignInButtonPress}
+					/>
+				</View>
 			</View>
-			<Divider />
-			<View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-				<GoogleSigninButton
-					size={GoogleSigninButton.Size.Wide}
-					color={GoogleSigninButton.Color.Light}
-					onPress={onGoogleButtonPress}
-				/>
-			</View>
-		</View>
+			<Snackbar visible={!!errorMessage} onDismiss={() => setErrorMessage(null)}>
+				{errorMessage}
+			</Snackbar>
+		</>
 	);
 };
 
