8a81f9a2c066ffcd9df13f28ab2da6179e1ae28a
Kegen Guyll <guyllkegen@gmail.com>
2024-06-13 14:25:20 -0500

    refactor: Add Snackbar for login prompt in HomeScreen

--- app/(tabs)/(movies,search,home,addReview,profile)/home/index.tsx
+++ app/(tabs)/(movies,search,home,addReview,profile)/home/index.tsx
@@ -1,18 +1,22 @@
 import React, { useMemo, useState } from 'react';
 import { FlatList, RefreshControl, View } from 'react-native';
-import { ActivityIndicator, Divider, Text } from 'react-native-paper';
+import { ActivityIndicator, Divider, Snackbar, Text } from 'react-native-paper';
+import { useRouter } from 'expo-router';
 import { useGetUserFeedQuery } from '@/redux/api/tmrev';
 import FeedCard from '@/features/feed/feedCard';
+import { loginRoute } from '@/constants/routes';
 
 const pageSize = 10;
 
 const HomeScreen = () => {
+	const router = useRouter();
 	const [isRefreshing, setIsRefreshing] = useState(false);
 	const [page, setPage] = useState(0);
 	const { data, isLoading, refetch } = useGetUserFeedQuery({
 		pageNumber: page,
 		pageSize,
 	});
+	const [loginMessage, setLoginMessage] = useState<string | null>('ss');
 
 	const hasReachedEnd = useMemo(() => data?.body.totalNumberOfPages === page, [data, page]);
 
@@ -38,25 +42,42 @@ const HomeScreen = () => {
 	}
 
 	return (
-		<View>
-			<FlatList
-				onEndReached={onEndReached}
-				refreshControl={
-					<RefreshControl tintColor="white" refreshing={isRefreshing} onRefresh={onRefresh} />
-				}
-				data={data.body.feed.reviews}
-				keyExtractor={(item) => item._id}
-				renderItem={({ item }) => (
-					<View>
-						<Divider />
-						<FeedCard contentType="reviews" review={item} from="home" />
-					</View>
-				)}
-				onEndReachedThreshold={0.5}
-				ListFooterComponentStyle={{ paddingTop: 16 }}
-				ListFooterComponent={hasReachedEnd ? null : <ActivityIndicator />}
-			/>
-		</View>
+		<>
+			<View>
+				<FlatList
+					onEndReached={onEndReached}
+					refreshControl={
+						<RefreshControl tintColor="white" refreshing={isRefreshing} onRefresh={onRefresh} />
+					}
+					data={data.body.feed.reviews}
+					keyExtractor={(item) => item._id}
+					renderItem={({ item }) => (
+						<View>
+							<Divider />
+							<FeedCard
+								setLoginMessage={setLoginMessage}
+								contentType="reviews"
+								review={item}
+								from="home"
+							/>
+						</View>
+					)}
+					onEndReachedThreshold={0.5}
+					ListFooterComponentStyle={{ paddingTop: 16 }}
+					ListFooterComponent={hasReachedEnd ? null : <ActivityIndicator />}
+				/>
+			</View>
+			<Snackbar
+				action={{
+					label: 'Login',
+					onPress: () => router.navigate(loginRoute()),
+				}}
+				visible={!!loginMessage}
+				onDismiss={() => setLoginMessage(null)}
+			>
+				{loginMessage}
+			</Snackbar>
+		</>
 	);
 };
 
