79943122b33e732a1fee670d543e8362fb34b021
Kegen Guyll <guyllkegen@gmail.com>
2024-06-13 14:28:47 -0500

    added messaged for reviewLoginPrompt and addToListLoginPrompt

--- constants/messages.ts
+++ constants/messages.ts
@@ -2,5 +2,14 @@ const likeLoginPrompt = 'Please login to like this post';
 const dislikeLoginPrompt = 'Please login to dislike this post';
 const commentLoginPrompt = 'Please login to comment';
 const errorPrompt = 'Error has occurred';
+const reviewLoginPrompt = 'Please login to review';
+const addToListLoginPrompt = 'Please login to add to list';
 
-export { likeLoginPrompt, dislikeLoginPrompt, commentLoginPrompt, errorPrompt };
+export {
+	likeLoginPrompt,
+	dislikeLoginPrompt,
+	commentLoginPrompt,
+	errorPrompt,
+	reviewLoginPrompt,
+	addToListLoginPrompt,
+};
