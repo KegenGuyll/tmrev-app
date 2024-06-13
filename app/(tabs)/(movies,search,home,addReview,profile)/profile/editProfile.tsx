1b31087dd6912636de5bd288ca6ac46e2ddd685c
Kegen Guyll <guyllkegen@gmail.com>
2024-06-12 20:57:37 -0500

    refactor: Add debounced username check and validation in EditProfile component

--- app/(tabs)/(movies,search,home,addReview,profile)/profile/editProfile.tsx
+++ app/(tabs)/(movies,search,home,addReview,profile)/profile/editProfile.tsx
@@ -1,9 +1,14 @@
 import { Stack, useRouter } from 'expo-router';
 import { View, StyleSheet } from 'react-native';
 import auth from '@react-native-firebase/auth';
-import { Button, Divider, TextInput } from 'react-native-paper';
-import { useEffect, useState } from 'react';
-import { useGetUserQuery, useUpdateUserMutation } from '@/redux/api/tmrev';
+import { Button, Divider, TextInput, Text } from 'react-native-paper';
+import { useEffect, useMemo, useState } from 'react';
+import {
+	useGetUserQuery,
+	useIsUsernameAvailableQuery,
+	useUpdateUserMutation,
+} from '@/redux/api/tmrev';
+import useDebounce from '@/hooks/useDebounce';
 
 const EditProfile = () => {
 	const { currentUser } = auth();
@@ -11,22 +16,34 @@ const EditProfile = () => {
 		{ uid: currentUser?.uid as string },
 		{ skip: !currentUser || !currentUser.uid }
 	);
+
 	const router = useRouter();
 	const [updateUser] = useUpdateUserMutation();
 
 	const [form, setForm] = useState({
-		username: data?.username,
-		location: data?.location,
-		bio: data?.bio,
+		username: data?.username || '',
+		location: data?.location || '',
+		bio: data?.bio || '',
 	});
+
+	const debouncedUsername = useDebounce(form.username, 200);
+
+	const { data: isUsernameAvailableData } = useIsUsernameAvailableQuery(debouncedUsername, {
+		skip: !debouncedUsername,
+	});
+
+	const doesUsernameMeetRequirements = useMemo(
+		() => form.username.length >= 5 && form.username.length <= 15,
+		[form.username]
+	);
 
 	useEffect(() => {
 		setForm({
-			username: data?.username,
-			location: data?.location,
-			bio: data?.bio,
+			username: data?.username || '',
+			location: data?.location || '',
+			bio: data?.bio || '',
 		});
-	}, []);
+	}, [data]);
 
 	const handleFormChange = (key: string, value: string) => {
 		setForm({ ...form, [key]: value });
@@ -57,13 +74,22 @@ const EditProfile = () => {
 				options={{ headerShown: true, title: `Edit Profile`, headerRight: () => null }}
 			/>
 			<View style={styles.container}>
-				<TextInput
-					mode="outlined"
-					label="Username"
-					onChange={(e) => handleFormChange('username', e.nativeEvent.text)}
-					value={form.username}
-					error={form.username === ''}
-				/>
+				<View>
+					<TextInput
+						error={!isUsernameAvailableData?.isAvailable || !doesUsernameMeetRequirements}
+						mode="outlined"
+						label="Username"
+						onChange={(e) => handleFormChange('username', e.nativeEvent.text)}
+						value={form.username}
+					/>
+					{isUsernameAvailableData?.success && !isUsernameAvailableData?.isAvailable && (
+						<Text variant="labelMedium">Username is already taken</Text>
+					)}
+					{!doesUsernameMeetRequirements && (
+						<Text variant="labelMedium">Username must be between 5 and 15 characters</Text>
+					)}
+				</View>
+
 				<TextInput
 					mode="outlined"
 					label="Location"
