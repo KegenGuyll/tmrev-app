ed9763a114e7f5efc52e1c0d8591c0785d600c29
Kegen Guyll <guyllkegen@gmail.com>
2024-06-13 14:25:10 -0500

    refactor: Add constants for login and error prompts

---
+++ constants/messages.ts
@@ -0,0 +1,6 @@
+const likeLoginPrompt = 'Please login to like this post';
+const dislikeLoginPrompt = 'Please login to dislike this post';
+const commentLoginPrompt = 'Please login to comment';
+const errorPrompt = 'Error has occurred';
+
+export { likeLoginPrompt, dislikeLoginPrompt, commentLoginPrompt, errorPrompt };
