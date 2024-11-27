efe44a814b9f7e4f0909545183c3b2d3cc3de3f2
Kegen Guyll <guyllkegen@gmail.com>
2024-11-27 15:00:51 -0600

    created global snackbar

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -8,11 +8,13 @@ import { peopleApi } from '../api/tmdb/peopleApi';
 import { searchApi } from '../api/tmdb/searchApi';
 import { tvApi } from '../api/tmdb/tvApi';
 import { tmrevApi } from '../api/tmrev';
+import { globalSnackbarSlice } from '../slice/globalSnackbar';
 
 const rootReducer = combineReducers({
 	counter: counterReducer,
 	bottomSheet: bottomSheetReducer,
 	userProfile: userProfileReducer,
+	globalSnackbar: globalSnackbarSlice.reducer,
 	[pokemonApi.reducerPath]: pokemonApi.reducer,
 	[movieApi.reducerPath]: movieApi.reducer,
 	[peopleApi.reducerPath]: peopleApi.reducer,
