b8808ba25d5d7a84bacebbabe711053321f22f90
Kegen Guyll <guyllkegen@gmail.com>
2024-11-27 11:38:09 -0600

    simplified the search and add review screens

--- app/(tabs)/(movies,search,home,addReview,profile)/search/index.tsx
+++ app/(tabs)/(movies,search,home,addReview,profile)/search/index.tsx
@@ -1,168 +1,103 @@
-import { Link } from 'expo-router';
-import React from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
-import { Chip, Divider, List, Searchbar, Text } from 'react-native-paper';
+import { router } from 'expo-router';
+import React, { useMemo } from 'react';
+import { View, TouchableHighlight } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { FlatGrid } from 'react-native-super-grid';
 import { useFindMoviesQuery, useFindPeopleQuery } from '@/redux/api/tmdb/searchApi';
-import MovieGrid from '@/components/MovieGrid';
 import ActorPlaceholderImage from '@/components/ActorPlacholderImage';
 import { personDetailsRoute } from '@/constants/routes';
-import MovieDiscoverGrid from '@/components/MovieDiscoverGrid';
 import useDebounce from '@/hooks/useDebounce';
+import { PeopleGeneral } from '@/models/tmdb/person';
+import { MovieGeneral } from '@/models/tmdb/movie/tmdbMovie';
+import MoviePoster from '@/components/MoviePoster';
+import { useGetMovieDiscoverQuery } from '@/redux/api/tmdb/movieApi';
+import { SortBy } from '@/models/tmdb/movie/movieDiscover';
 
-type SearchSelection = 'movies' | 'crew';
-
-type SearchSelectionChipProps = {
-	label: string;
-	onPress: () => void;
-	isSelected: boolean;
+type SearchRenderItemProps = {
+	item: PeopleGeneral | MovieGeneral;
 };
 
-const SearchSelectionChip: React.FC<SearchSelectionChipProps> = ({
-	label,
-	onPress,
-	isSelected,
-}) => {
-	return (
-		<Chip onPress={onPress} mode={!isSelected ? 'outlined' : 'flat'}>
-			<Text>{label}</Text>
-		</Chip>
-	);
-};
-
-type SearchBarComponentProps = {
-	searchQuery: string;
-	setSearchQuery: (query: string) => void;
-	setSearchSelection: (selection: SearchSelection) => void;
-	searchSelection: SearchSelection;
-};
-
-const SearchBarComponent: React.FC<SearchBarComponentProps> = ({
-	searchQuery,
-	setSearchQuery,
-	searchSelection,
-	setSearchSelection,
-}: SearchBarComponentProps) => {
-	return (
-		<View style={{ paddingVertical: 16, width: '100%', flexGrow: 1 }}>
-			<Searchbar
-				style={{ width: '100%', flexGrow: 1 }}
-				placeholder="Search..."
-				value={searchQuery}
-				onChangeText={(t) => setSearchQuery(t)}
-			/>
-			<View style={styles.chipContainer}>
-				<SearchSelectionChip
-					label="Movies"
-					onPress={() => setSearchSelection('movies')}
-					isSelected={searchSelection === 'movies'}
-				/>
-				<SearchSelectionChip
-					label="Cast, Crew or Studios"
-					onPress={() => setSearchSelection('crew')}
-					isSelected={searchSelection === 'crew'}
-				/>
+const SearchRenderItem: React.FC<SearchRenderItemProps> = ({ item }: SearchRenderItemProps) => {
+	if ('profile_path' in item) {
+		return (
+			<View>
+				<TouchableHighlight
+					onPress={() => router.navigate(personDetailsRoute('search', item.id.toString()))}
+				>
+					<ActorPlaceholderImage
+						profile_url={item.profile_path}
+						department={item.known_for_department}
+						height={125}
+						width={75}
+					/>
+				</TouchableHighlight>
 			</View>
+		);
+	}
+
+	return (
+		<View>
+			<MoviePoster
+				movieId={item.id}
+				moviePoster={item.poster_path}
+				location="search"
+				height={125}
+				width={75}
+			/>
 		</View>
 	);
 };
 
 const Search: React.FC = () => {
 	const [searchQuery, setSearchQuery] = React.useState('');
-	const [searchSelection, setSearchSelection] = React.useState<SearchSelection>('movies');
 
 	const debouncedSearchTerm = useDebounce(searchQuery, 500);
 
-	const { data: movieData, isFetching: movieIsFetching } = useFindMoviesQuery(
-		{ query: debouncedSearchTerm },
-		{ skip: searchSelection !== 'movies' }
+	const { data: movieData } = useFindMoviesQuery({
+		query: debouncedSearchTerm,
+	});
+
+	const { data: peopleData } = useFindPeopleQuery({ query: debouncedSearchTerm });
+
+	const { data: discoverData } = useGetMovieDiscoverQuery(
+		{
+			params: {
+				language: 'en-US',
+				sort_by: SortBy.MOST_POPULAR,
+				include_adult: false,
+				include_video: false,
+				page: 1,
+			},
+		},
+		{ skip: !!searchQuery }
 	);
 
-	const { data: peopleData } = useFindPeopleQuery(
-		{ query: debouncedSearchTerm },
-		{ skip: searchSelection !== 'crew' }
-	);
+	const results = useMemo(() => {
+		if (!movieData?.results?.length && !peopleData?.results?.length) {
+			return discoverData?.results ?? [];
+		}
+		const combinedResults = [...(movieData?.results ?? []), ...(peopleData?.results ?? [])];
+		return combinedResults.sort((a, b) => (b.popularity ?? 0) - (a.popularity ?? 0));
+	}, [movieData, peopleData, discoverData]);
 
 	return (
 		<View>
-			{!searchQuery && searchSelection === 'movies' && (
-				<MovieDiscoverGrid
-					imageHeight={125}
-					itemDimension={75}
-					ListHeaderComponent={
-						<SearchBarComponent
-							searchQuery={searchQuery}
-							setSearchQuery={setSearchQuery}
-							searchSelection={searchSelection}
-							setSearchSelection={setSearchSelection}
-						/>
-					}
-					from="search"
-				/>
-			)}
-			{movieData && searchSelection === 'movies' && (
-				<MovieGrid
-					ListHeaderComponent={
-						<SearchBarComponent
-							searchQuery={searchQuery}
-							setSearchQuery={setSearchQuery}
-							searchSelection={searchSelection}
-							setSearchSelection={setSearchSelection}
-						/>
-					}
-					imageHeight={125}
-					itemDimension={75}
-					movies={movieData.results}
-					isLoading={movieIsFetching}
-					onEndReached={() => {}}
-					onEndReachedThreshold={1}
-					posterPath="search"
-					bottomPadding={100}
-				/>
-			)}
-			{peopleData && searchSelection === 'crew' && (
-				<FlatList
-					style={{ width: '100%' }}
-					ListHeaderComponent={
-						<SearchBarComponent
-							searchQuery={searchQuery}
-							setSearchQuery={setSearchQuery}
-							searchSelection={searchSelection}
-							setSearchSelection={setSearchSelection}
-						/>
-					}
-					data={peopleData.results}
-					renderItem={({ item }) => (
-						<>
-							<Link href={personDetailsRoute('search', String(item.id))}>
-								<List.Item
-									title={item.name}
-									left={() => (
-										<ActorPlaceholderImage
-											profile_url={item.profile_path}
-											department={item.known_for_department}
-											height={50}
-											width={50}
-										/>
-									)}
-									description={item.known_for_department}
-								/>
-							</Link>
-							<Divider />
-						</>
-					)}
-					keyExtractor={(item) => item.id.toString()}
-				/>
-			)}
+			<FlatGrid
+				itemDimension={75}
+				ListHeaderComponentStyle={{ paddingVertical: 12 }}
+				ListHeaderComponent={
+					<Searchbar
+						style={{ width: '100%', flexGrow: 1 }}
+						placeholder="Search Movies or People..."
+						value={searchQuery}
+						onChangeText={(t) => setSearchQuery(t)}
+					/>
+				}
+				data={results}
+				renderItem={({ item }) => <SearchRenderItem item={item} />}
+			/>
 		</View>
 	);
 };
-
-const styles = StyleSheet.create({
-	chipContainer: {
-		flexDirection: 'row',
-		gap: 8,
-		marginTop: 8,
-	},
-});
 
 export default Search;
