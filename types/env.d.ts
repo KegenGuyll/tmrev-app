176773a5bd634b0619ac8f02f8b7a7829b325126
Kegen Guyll <guyllkegen@gmail.com>
2025-12-14 13:45:18 -0600

    Refactor code structure for improved readability and maintainability

--- types/env.d.ts
+++ types/env.d.ts
@@ -3,4 +3,5 @@ declare module '@env' {
 	export const TMDB_API_KEY: string;
 	export const GOOGLE_WEB_CLIENT_ID: string;
 	export const TMREV_API_URL: string;
+	export const TMREV_V2_API_URL: string;
 }
