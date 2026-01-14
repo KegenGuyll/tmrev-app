b05e34cc8bc07ac00079c42b58ecf379169f766b
Kegen Guyll <guyllkegen@gmail.com>
2026-01-14 15:51:44 -0600

    fix: add predicate to invalidate queries for watched movie lists based on movieId

--- features/movieDetails/WatchedMovie.tsx
+++ features/movieDetails/WatchedMovie.tsx
@@ -10,6 +10,7 @@ import {
 	useWatchedControllerCreate,
 	useWatchedControllerGetWatchedStatus,
 	useWatchedControllerUpdate,
+	WatchlistAggregatedDetail,
 } from '@/api/tmrev-api-v2';
 import { likeMovieLoginPrompt } from '@/constants/messages';
 
@@ -43,6 +44,27 @@ const WatchedMovie: React.FC<WatchedMovieProps> = ({
 			queryClient.invalidateQueries({
 				queryKey: getWatchedControllerFindByUserIdQueryKey(currentUser?.uid || ''),
 				exact: false,
+			}),
+			queryClient.invalidateQueries({
+				predicate: (query) => {
+					const [key] = query.queryKey;
+					if (typeof key !== 'string') return false;
+
+					// Handle individual list details
+					if (key.startsWith('/watch-list/')) {
+						const data: WatchlistAggregatedDetail | undefined = queryClient.getQueryData(
+							query.queryKey
+						);
+
+						if (!data) return false;
+
+						// Check if this specific list object contains the movieId
+						const foundMovieInList = data?.movies?.some((m) => m.id === movieId);
+						return foundMovieInList;
+					}
+
+					return false;
+				},
 			}),
 		]);
 	};
