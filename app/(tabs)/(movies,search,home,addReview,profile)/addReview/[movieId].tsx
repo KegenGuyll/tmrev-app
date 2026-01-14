ffe675b2add63e4115136c07a83a279344ab608e
Kegen Guyll <guyllkegen@gmail.com>
2026-01-14 15:57:34 -0600

    fix: add query invalidation for watchlist when creating or updating reviews

--- app/(tabs)/(movies,search,home,addReview,profile)/addReview/[movieId].tsx
+++ app/(tabs)/(movies,search,home,addReview,profile)/addReview/[movieId].tsx
@@ -23,6 +23,7 @@ import {
 	useReviewControllerFindByTmdbId,
 	useReviewControllerFindOne,
 	useReviewControllerUpdate,
+	WatchlistAggregatedDetail,
 } from '@/api/tmrev-api-v2';
 
 type CreateReviewSearchParams = {
@@ -74,6 +75,27 @@ const CreateReview = () => {
 				queryClient.invalidateQueries({
 					queryKey: getFeedControllerGetFeedQueryKey(),
 					exact: false,
+				});
+				queryClient.invalidateQueries({
+					predicate: (query) => {
+						const [key] = query.queryKey;
+						if (typeof key !== 'string') return false;
+
+						// Handle individual list details
+						if (key.startsWith('/watch-list/')) {
+							const data: WatchlistAggregatedDetail | undefined = queryClient.getQueryData(
+								query.queryKey
+							);
+
+							if (!data) return false;
+
+							// Check if this specific list object contains the movieId
+							const foundMovieInList = data?.movies?.some((m) => m.id === Number(movieId));
+							return foundMovieInList;
+						}
+
+						return false;
+					},
 				});
 			},
 		},
